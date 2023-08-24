11c8b8e9755e89c7a4825b1eb410ac9e35ea7a3e
Boney Labinghisa <blabinghisa@yahoo.com>
2023-08-24 14:36:06 +0100

    added bs4 code

--- dags/main.py
+++ dags/main.py
@@ -1,6 +1,11 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
+
+from bs4 import BeautifulSoup
+import requests
+import csv
+
 # from data import process_csv
 import sys
 
@@ -20,6 +25,30 @@ def process_csv():
     local_file_path = '/opt/airflow/include/general_data.csv'
     s3.upload_file(local_file_path, s3_bucket, 'general_data.csv')
 
+def web_scraping_b4():
+    URL = "https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops"
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    products=[]  # a list to store quotes
+   
+    table = soup.find('div', attrs = {'class':'row'}) 
+   
+    for row in table.findAll('div', 
+                             attrs = {"class":"col-sm-4 col-lg-4 col-md-4"}):
+        cols = row.find_all('thumbnail')
+        item = cols.text
+        price = row.a['href']
+        description = row.img['src']
+        rating = row.img['alt'].split(" #")[0]
+        products.append([item,price,description,rating])
+   
+filename = 'inspirational_quotes.csv'
+with open(filename, 'w', newline='') as f:
+    w = csv.DictWriter(f,['theme','url','img','lines','author'])
+    w.writeheader()
+    for quote in quotes:
+        w.writerow(quote)
 
 with DAG("my_dag", start_date=datetime(2021, 1, 1),
     schedule_interval="@daily", catchup=False) as dag:
