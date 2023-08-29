9f0b0c476d9b2a89326edd45aec92780df76e91f
Boney Labinghisa <blabinghisa@yahoo.com>
2023-08-29 14:38:56 +0100

    commenting out main functions

--- dags/main.py
+++ dags/main.py
@@ -5,29 +5,29 @@ from airflow.operators.bash import BashOperator
 from bs4 import BeautifulSoup
 import requests
 import csv
-
-# from data import process_csv
 import sys
 
 sys.path.append("./include")
 from streamlitapp import visualize_data
 
-# from random import randint
 from datetime import datetime
 import boto3
 
+'''
+    # The function process_csv uploads the 'products.csv' from web_scraping_b4 function to Amazon S3 bucket.
+    # Simply add this to the DAG as a PythonOperator and schedule it after web scraping data.
 
 def process_csv():
     s3 = boto3.client('s3',aws_access_key_id='ssss',
     aws_secret_access_key='ssss')
     s3_bucket = 'xander-test-c8'
     # s3_key = 'path/to/your/csv/file.csv'
-    local_file_path = '/opt/airflow/include/general_data.csv'
-    s3.upload_file(local_file_path, s3_bucket, 'general_data.csv')
+    local_file_path = '/opt/airflow/include/products.csv'
+    s3.upload_file(local_file_path, s3_bucket, 'products.csv')
 
-'''
+
     # The function web_scraping_b4 below is an example of a bs4 web scraping script. You can use this as a reference in making you webscraping
-    # function. Simple add this to the DAG as a PythonOperator and schedule it before the processing of the csv if needed.  
+    # function. Simply add this to the DAG as a PythonOperator and schedule it before the processing of the csv if needed.  
 
 def web_scraping_b4():
     URL = "https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops"
@@ -43,9 +43,9 @@ def web_scraping_b4():
     table_new = table.find('div', attrs = {'class':"row"})
     rows = table_new.find_all('div', attrs = {'class':"caption"})
 
-    print(rows)
+    # print(rows)
 
-    print("-------------------------------------")
+    # print("-------------------------------------")
     filename = 'products.csv'
     with open(filename, 'w', newline='') as f:
         w = csv.DictWriter(f,['item','price'])
@@ -62,6 +62,11 @@ def web_scraping_b4():
 
 with DAG("my_dag", start_date=datetime(2021, 1, 1),
     schedule_interval="@daily", catchup=False) as dag:
+    
+        webscrape_data_task = PythonOperator(
+            task_id='web_scrape_data',
+            python_callable=web_scraping_b4
+        )
 
         process_csv_task = PythonOperator(
             task_id='process_csv',
@@ -72,10 +77,6 @@ with DAG("my_dag", start_date=datetime(2021, 1, 1),
             task_id='visualize_data',
             python_callable=visualize_data
         )
-        # run_this = BashOperator(
-        #     task_id="run_streamlit",
-        #     bash_command="streamlit run --server.enableWebsocketCompression=false --server.enableCORS=false --server.enableXsrfProtection=false /opt/airflow/include/streamlitapp.py",
-        # )
 
-        process_csv_task >> visualize_data_task
+        webscrape_data_task >> process_csv_task >> visualize_data_task
 
--- include/streamlitapp.py
+++ include/streamlitapp.py
@@ -12,17 +12,17 @@ def visualize_data():
     s3_bucket = 'xander-test-c8'
     local_file_name = 'new_data.csv'
     with open(local_file_name, 'wb') as f:
-        s3.download_fileobj(s3_bucket, 'general_data.csv', f)
+        s3.download_fileobj(s3_bucket, 'products.csv', f)
 
-    data = pd.read_csv('new_data.csv')
+    data = pd.read_csv('products.csv')
 
     st.dataframe(data)
     st.title("Testing Streamlit")
 
-    options = ["MonthlyIncome","TotalWorkingYears","JobRole"]
-    selected = st.selectbox("Select an Employee data value", options)
-    inform = f"Employees {selected} Chart:"
-    fig = px.bar(data, x="Age", y=selected, title=inform)
+    options = ["item","price"]
+    selected = st.selectbox("Select an Item", options)
+    inform = f"Item {selected} Chart:"
+    fig = px.bar(data, x="item", y=selected, title=inform)
     st.plotly_chart(fig, use_container_width=True)
 
     print("Streamlit successful!!!")
